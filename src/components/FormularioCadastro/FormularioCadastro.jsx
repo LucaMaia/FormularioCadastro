9272153549362dc6954201c8bdad42e35cc629e1
Luca Lins Maia Vieira De Oliveira <lucaoliveira@mec.gov.br>
2021-10-28 09:36:19 -0300

    Inicio

---
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+
+function FormularioCadastro() {
+
+return(
+    <form>
+        <TextField id="nome" label="Nome" variant="outlined" margin ="normal" fullWidth/>
+
+        <TextField id="Sobrenome" label="Sobrenome" variant="outlined" margin ="normal" fullWidth/>
+        
+        <TextField id="CPF" label="CPF" variant="outlined" margin ="normal" fullWidth/>
+
+        <label>Promoções</label>
+        <input type="checkbox"/>
+
+        <label>Novidades</label>
+        <input type="checkbox"/>
+
+        <Button type="submit" variant="contained" color="primary">
+            Cadastrar
+        </Button>
+    </form>
+    );
+}
+
+
+export default FormularioCadastro;
